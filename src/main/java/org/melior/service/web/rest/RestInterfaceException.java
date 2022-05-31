fbddf9f6dcc9066afd6e7e222019a791fd950f6e
MeliorArtefacts <106606719+MeliorArtefacts@users.noreply.github.com>
2022-05-31 19:16:06 +0200

    Add files via upload

---
+++ src/main/java/org/melior/service/web/rest/RestInterfaceException.java
@@ -0,0 +1,153 @@
+/* __  __      _ _            
+  |  \/  |    | (_)           
+  | \  / | ___| |_  ___  _ __ 
+  | |\/| |/ _ \ | |/ _ \| '__|
+  | |  | |  __/ | | (_) | |   
+  |_|  |_|\___|_|_|\___/|_|   
+        Service Harness
+*/
+package org.melior.service.web.rest;
+import org.melior.service.exception.ExceptionType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * TODO
+ * @author Melior
+ * @since 2.0
+ */
+public class RestInterfaceException extends Exception
+{
+		private HttpStatus status;
+
+		private ExceptionType type;
+
+		private String code;
+
+	/**
+	 * Constructor.
+	 */
+	public RestInterfaceException()
+	{
+				super();
+	}
+
+	/**
+	 * Constructor.
+	 * @param type The exception type
+	 * @param code The exception code
+	 * @param message The exception message
+	 */
+	public RestInterfaceException(
+		final ExceptionType type,
+		final String code,
+		final String message)
+	{
+				this(null, type, code, message);
+	}
+
+	/**
+	 * Constructor.
+	 * @param status The response status
+	 * @param type The exception type
+	 * @param code The exception code
+	 * @param message The exception message
+	 */
+	public RestInterfaceException(
+		final HttpStatus status,
+		final ExceptionType type,
+		final String code,
+		final String message)
+	{
+				super(message);
+
+				this.status = (status == null) ? getStatus(type) : status;
+
+				this.type = type;
+
+				this.code = code;
+	}
+
+	/**
+	 * Get response status.
+	 * @return The response status
+	 */
+	public HttpStatus getStatus()
+	{
+		return status;
+	}
+
+	/**
+	 * Get exception type.
+	 * @return The exception type
+	 */
+	public ExceptionType getType()
+	{
+		return type;
+	}
+
+	/**
+	 * Get exception code.
+	 * @return The exception code
+	 */
+	public final String getCode()
+	{
+		return code;
+	}
+
+	/**
+	 * Get response status.
+	 * @param type The exception type
+	 * @return The response status
+	 */
+	private HttpStatus getStatus(
+		final ExceptionType type)
+	{
+
+				switch (type)
+		{
+		case NO_DATA:
+			return HttpStatus.NOT_FOUND;
+		case SERVICE_OVERLOAD:
+			return HttpStatus.TOO_MANY_REQUESTS;
+		case SERVICE_UNAVAILABLE:
+		case DATAACCESS_COMMUNICATION:
+			return HttpStatus.SERVICE_UNAVAILABLE;
+		default:
+			return HttpStatus.BAD_REQUEST;
+		}
+
+	}
+
+	/**
+	 * Get response.
+	 * @return The response
+	 */
+	public ResponseEntity<ExceptionPayload> getResponse()
+	{
+				return new ResponseEntity<ExceptionPayload>(new ExceptionPayload(type.name(), code, getMessage()), status);
+	}
+
+	/**
+	 * Throw checked exception as runtime exception.
+	 * @param exception The checked exception
+	 */
+	public static void throwRuntimeException(
+		final Exception exception)
+	{
+		RestInterfaceException.<RuntimeException> throwException(exception);
+	}
+
+	/**
+	 * Throw exception.
+	 * @param exception The exception
+	 * @throws E The exception
+	 */
+	@SuppressWarnings("unchecked")
+	private static <E extends Exception> void throwException(
+		final Exception exception) throws E
+	{
+		throw (E) exception;
+	}
+
+}
---
+++ src/main/java/org/melior/service/web/rest/RestService.java
@@ -0,0 +1,47 @@
+/* __  __      _ _            
+  |  \/  |    | (_)           
+  | \  / | ___| |_  ___  _ __ 
+  | |\/| |/ _ \ | |/ _ \| '__|
+  | |  | |  __/ | | (_) | |   
+  |_|  |_|\___|_|_|\___/|_|   
+        Service Harness
+*/
+package org.melior.service.web.rest;
+import org.melior.context.service.ServiceContext;
+import org.melior.service.core.AbstractService;
+import org.melior.service.exception.ApplicationException;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * TODO
+ * @author Melior
+ * @since 2.0
+ */
+@RestController
+public abstract class RestService extends AbstractService
+{
+
+	/**
+	 * Bootstrap service.
+	 * @param serviceClass The service class
+	 * @param args The command line arguments
+	 */
+	public static void run(
+		final Class<?> serviceClass,
+		final String[] args)
+	{
+				AbstractService.run(serviceClass, args, true);
+	}
+
+	/**
+	 * Constructor.
+	 * @param serviceContext The service context
+	 * @throws ApplicationException when the initialization fails
+	 */
+	public RestService(
+		final ServiceContext serviceContext) throws ApplicationException
+	{
+				super(serviceContext, true);
+	}
+
+}
